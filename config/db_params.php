ef5324b719fcf59ddd747b7ac96d48dab05cdc54
zarpom <vyacheslavxxxxx@gmail.com>
2017-10-19 21:38:50 +0300

    second commit

---
+++ config/db_params.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'host' => 'guestbook',
+    'dbname' => 'guestbook',
+    'user' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+);
---
+++ config/routes.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'message\/([0-9]+)' => 'message/view/$2',
+    'message\/p-([0-9]+)' => 'message/index/$1',
+    'message' => 'message/index',
+    'user/register' => 'user/register',
+    'user/login' => 'user/login',
+    'cabinet' => 'cabinet/index'
+);
+
+?>
